8e3edff8a8c641e942c8d871a9525fd3a6319c40
nocomraphaella <160980300+nocomraphaella@users.noreply.github.com>
2024-08-18 20:11:41 -0700

    Add files via upload

---
+++ Activity_Nocom/arithmetic_raphaella.php
@@ -0,0 +1,21 @@
+<?php
+  $number1 = 10;
+  $number2 = 20;
+  $number3 = 30;
+  $number4 = 40;
+
+  $a = $number1 + $number2;
+  $b = $number3 - $number2;
+  $c = $number1 * $number4;
+  $d = $number4 / $number2;
+  $e = $a + $b + $c + $d;
+  $f = $e / 4;
+
+
+  echo "The sum of $number1 and $number2 is $a <br>";
+  echo "<br> The difference of $number3 and $number2 is $b <br>";
+  echo "<br> The product of $number1 and $number4 is $c <br>";
+  echo "<br> The division of $number4 and $number2 is $d <br>";
+  echo "<br> The total of all results is $e <br>";
+  echo "<br> The averange of all results is $f";
+?>
---
+++ Activity_Nocom/variables_raphaella.php
@@ -0,0 +1,7 @@
+<?php
+  $name = "Raphaella Laurraine Nocom";
+  $age = 19;
+  $favorite_color = "pink";
+
+  echo "My name is $name, I am $age years old, and my favorite color is $favorite_color.";
+?>
